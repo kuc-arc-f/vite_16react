32e3c816e164dbdc024551ba56210ddb4750e6af
中島浩二 <knaka0209@gmail.com>
2024-06-17 21:44:35 +0900

    up, 0617a5

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-import './App.css'
+//import './App.css'
 import { Link, Route, Routes } from 'react-router-dom'
 
 // Auto generates routes from files under ./pages
